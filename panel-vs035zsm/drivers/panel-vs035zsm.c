b37a46e04fb4a5eb109a99965fe9df753ec08bf4
Reese Chong <dev.reese.chong@gmail.com>
2025-11-02 22:49:44 +0000

    wip panel mvp

---
+++ panel-vs035zsm/drivers/panel-vs035zsm.c
@@ -0,0 +1,86 @@
+// MVP for display driver
+
+#include <linux/module.h>
+#include <linux/of.h>
+#include <drm/drm_panel.h>
+#include <drm/drm_mipi_dsi.h>
+#include <drm/drm_modes.h>
+
+// context??
+struct vs035_ctx {
+	struct drm_panel panel;
+	struct mipi_dsi_device *dsi;
+	bool prepared;
+};
+
+// container_of: get pointer of parent when you have the pointer of a child
+// inline: find-and-replace function body instead of function call
+static inline struct vs035_ctx *to_ctx(struct drm_panel *p) {
+	return container_of(p, struct vs035_ctx, panel);
+}
+
+// these functions don’t actually do anything, but they exist because the DRM panel driver framework expects them
+// (drm_panel_funcs uses them)
+static int vs035_prepare(struct drm_panel *panel){i
+	struct vs035_ctx *ctx = to_ctx(panel);
+	if (ctx->prepared) return 0;
+	ctx->prepared = true;
+	return 0;
+}
+
+static int vs035_unprepared(struct drm_panel *panel){
+	struct vs035_ctx *ctx = to_ctx(panel);
+	if (!ctx->prepared) return 0;
+	ctx->prepared = false;
+	return 0;
+}
+
+static int vs035_enable(struct drm_panel *panel) {return 0; }
+
+static int vs035_disable(struct drm_panel *panel) { return 0; }
+
+// placeholder porches/clock
+static int vs035_get_modes(struct drm_panel *panel, struct drm_connector *connector) {
+	struct drm_display_node *m = drm_mode_create(connector->dev);
+	if (!m) return 0;
+
+	m->display	= 1440;
+	m->hsync_start	= 1440 + 16; /* HFP */
+	m->hsync_end	= m->hsync_start + 8; /* HSYNC */
+	m->htotal	= m->hsync_end + 16; /* HBP */
+
+	m->vdisplay	= 1600;
+	m->vsync_start	= 1600 + 8; /* VFP */
+	m->vsync_end	= m->vsync_start + 4;
+	m->vtotal	= m->vsync_end + 8;
+
+	m->clock	= (m->htotal * m->vtotal * 60) / 1000; /* 60Hz */
+
+	m->type = DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED;
+	drm_mode_set_name(m);
+	drm_mode_probed_add(connector, m);
+	return 1;
+}
+
+static const struct drm_panel_funcs vs035_funcs = {
+	.prepare	= vs035_prepare,
+	.unprepare	= vs035_unprepare,
+	.enable		= vs035_enable,
+	.disable	= vs035_disable,
+	.get_modes	= vs035_get_modes,
+}
+
+static int vs035_probe(struct mipi_dsi_device *dsi) {}
+
+static void vs035_remove(struct mipi_dsi_device *dsi) {}
+
+static const struct of_device_id vs035_of_match[] = {}
+
+MODULE_DEVICE_TABLE(of, vs035_of_match);
+
+static struct mipi_dsi_driver vs035_driver = {};
+
+module_mipi_dsi_driver(vs035_driver);
+
+MODULE_DESCRIPTION("VS035ZSM ultra-minimal DRM MIPI-DSI panel");
+MODULE_LICENSE("GPL");
